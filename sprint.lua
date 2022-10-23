76d14b188a942c40a0a07323e3f7b27f8fd941eb
picklelolxd <48166748+picklelolxd@users.noreply.github.com>
2022-10-23 11:31:41 +0200

    Create sprint.lua

---
+++ sprint.lua
@@ -0,0 +1,31 @@
+local userInput = game:GetService ('UserInputService')
+local Players = game:GetService ('Players')
+local sprintSpeed = 30
+local walkSpeed = 16
+local player = Players.LocalPlayer
+
+local function beginSprint (input, gameProcessed)
+   if not gameProcessed then
+       if input.UserInputType == Enum.UserInputType.Keyboard then
+           local keycode = input.KeyCode
+           if keycode == Enum.KeyCode.LeftShift then
+               player.Character.Humanoid.WalkSpeed = sprintSpeed
+           end
+       end
+   end
+end
+
+local function endSprint (input, gameProcessed)
+   if not gameProcessed then
+       if input.UserInputType == Enum.UserInputType.Keyboard then
+           local keycode = input.KeyCode
+           if keycode == Enum.KeyCode.LeftShift then
+               player.Character.Humanoid.WalkSpeed = walkSpeed
+           end
+       end
+   end
+end
+
+
+userInput.InputBegan:Connect (beginSprint)
+userInput.InputEnded:Connect (endSprint)
